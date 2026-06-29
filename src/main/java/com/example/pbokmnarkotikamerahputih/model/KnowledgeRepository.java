5e83e07a89cd4bbd1128b42923607e4c2e2075d3
Muh Rizal Julfikar <muhrizaljulfikargmailcom@webmail.umm.ac.id>
2026-06-29 11:14:39 +0700

    feat: tambah struktur dasar KnowledgeRepository

--- src/main/java/com/example/pbokmnarkotikamerahputih/model/KnowledgeRepository.java
+++ src/main/java/com/example/pbokmnarkotikamerahputih/model/KnowledgeRepository.java
@@ -9,31 +9,4 @@ public class KnowledgeRepository {
     public KnowledgeRepository() {
         daftarPutusan = new ArrayList<>();
     }
-
-    public void tambahPutusan(Putusan putusan) {
-        if (putusan != null) {
-            daftarPutusan.add(putusan);
-        }
-    }
-
-    public List<Putusan> getSemuaPutusan() {
-        return new ArrayList<>(daftarPutusan);
-    }
-
-    public Putusan cariBerdasarkanNomor(String nomor) {
-        for (Putusan p : daftarPutusan) {
-            if (p.getNomorPerkara().equalsIgnoreCase(nomor)) {
-                return p;
-            }
-        }
-        return null;
-    }
-
-    public boolean hapusPutusan(String nomor) {
-        return daftarPutusan.removeIf(p -> p.getNomorPerkara().equalsIgnoreCase(nomor));
-    }
-
-    public int getJumlahData() {
-        return daftarPutusan.size();
-    }
 }
