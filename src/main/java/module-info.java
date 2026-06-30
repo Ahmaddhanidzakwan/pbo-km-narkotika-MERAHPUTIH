07409c9b2c271a28d65c17059a213e8e7b1bb491
Dzakwan <Daniawan950@gmail.com>
2026-06-30 08:42:15 +0700

    Tambah class StatistikView + fix:moduleinfojava

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module com.example.pbokmnarkotikamerahputih {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
+    exports com.example.pbokmnarkotikamerahputih.app;
+    exports com.example.pbokmnarkotikamerahputih.view;
+    exports com.example.pbokmnarkotikamerahputih.controller;
+    exports com.example.pbokmnarkotikamerahputih.model;
+    exports com.example.pbokmnarkotikamerahputih.util;
 
-    opens com.example.pbokmnarkotikamerahputih to javafx.fxml;
-    exports com.example.pbokmnarkotikamerahputih;
-    exports com;
-    opens com to javafx.fxml;
+    opens com.example.pbokmnarkotikamerahputih.view to javafx.graphics, javafx.fxml;
+    opens com.example.pbokmnarkotikamerahputih to javafx.graphics;
 }
